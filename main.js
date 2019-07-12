64b4017ead80a6daeafce1b0e77fd9ce85886303
yjt1212 <49463417+yjt1212@users.noreply.github.com>
2019-07-12 17:07:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// 项目入口
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -9,7 +8,6 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
-// 提示信息，不搭理它
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
